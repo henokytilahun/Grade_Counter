68c401bf2d2e2cb482a66bd0f5043a1bbc40d0a7
henokytilahun <109305666+henokytilahun@users.noreply.github.com>
2022-09-21 00:51:14 +0000

    blue print

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main()
+{
+
+  //initalize variables
+  int a_s = 0; //A counter for Sam
+  int a_l = 0; //A conunter for Lydia
+
+
+  //ask for Sam's grade to be put in
+
+
+  //ask for the number of A's this quarter
+
+
+  //Calculate the money made
+
+
+  //ask for Lydia's grade to be put in
+  
+
+  //ask for the number of A's this quarter
+
+  
+  //Calculate the money made
+
+
+  //write money made into .txt file
+
+
+
+  //transfer .txt file to MySQL
+
+  return 0;
+}
